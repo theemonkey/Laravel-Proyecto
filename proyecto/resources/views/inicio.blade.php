9d4c72700e55c5ae2de447bfcc90c18a4b7576e8
yamit <yamit.aguirre@utp.edu.co>
2025-07-17 16:00:42 -0500

    version inicial

---
+++ proyecto/resources/views/inicio.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+
+    Bienvenidos a Laravel 11 
+    Esta es una pagina de inicio
+    
+</body>
+</html>
